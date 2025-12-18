90537f6faecfa93b753818e161d66c9476225351
agrawal-aditi18 <aditiagrawalsofficial@gmail.com>
2025-12-18 09:16:42 +0530

    built toggle sidebar

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,17 +1,28 @@
+import { useDispatch } from "react-redux"
 import searchLogo from "../search-logo.png"
+import { toggleMenu } from "../utils/appSlice";
 
 const Head = () => {
+  const dispatch = useDispatch();
+
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  }
+
   return (
     <div className="grid grid-flow-col p-2 m-2 shadow-lg ">
         <div className="flex col-span-1">
             <img 
-            className='h-7'
+            onClick={() => toggleMenuHandler()}
+            className="h-7 cursor-pointer"
             alt = "menu"
             src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr9B9snrsfNCLmPN8SK8WARBp_ENIItDvXcg&s" />
+            <a href = "/">
             <img 
             className='h-7 mx-3'
             alt = "you-tube logo"
             src = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png"/>
+            </a>
         </div>
         <div className='col-span-1 flex'>
             <input className='w-5/6 rounded-l-full border border-gray-400 p-2'  type ="text" placeholder ="Search"/>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Sidebar = () => {
+const isMenuOpen = useSelector(store => store.app.isMenuOpen)
+//Early return pattern
+if(!isMenuOpen) return null;
+
   return (
     <div className='w-50 p-5 shadow-lg'>
       <ul>
